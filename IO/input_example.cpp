f0dd208fb7568b194c2c8330430bd8f0760a3c07
lmlepin9 <luis.moralepin@postgrad.manchester.ac.uk>
2023-02-11 21:47:46 +0000

    Including string validation

--- IO/input_example.cpp
+++ IO/input_example.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <limits>
+#include <string> 
 
 
 int GetInterger(){
-
      /*
-    This works like this:
+    This function requests a integer to the user and cheks its validity 
+
+    It works like this:
     First (cin.clear) we reset the error flag of the cin object 
     Second (cin.ignore) we clean the cin buffer (i.e: whatever it was the user input)
+    Also it checks wether there are any leftovers after the integer in case the user
+    inputs a float 
     
     */ 
-
     int my_interger;
     while ( ( std::cout << "Give me an interger greater than zero " ) && ( !( std::cin >> my_interger ) || my_interger < 0  || std::cin.get() != '\n' ) ) {
         std::cout << "That's not an interger, try again please" << std::endl; 
@@ -19,16 +22,30 @@ int GetInterger(){
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n' );
 
     }
-
     return my_interger; 
 }
 
+std::string GetYesorNo(){
+    std::string your_answer;
+    while((std::cout << "Answer yes or no ") && ( !(std::cin >> your_answer) || (your_answer != "yes" && your_answer != "no") ) ){
+        std::cout << "Please, just answer yes or no, try again" << std::endl;
+        std::cout << "\n" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    
+    } 
+    return your_answer;
+}
+
+// V and (V or (F and F) )
 
 int main(){
 
 
     int input_int = GetInterger(); 
-    std::cout << "Your input is: " << input_int << std::endl;
+    std::cout << "Your interger is: " << input_int << std::endl;
+    std::string input_yn = GetYesorNo() ; 
+    std::cout << "You answer is: " << input_yn << std::endl;
     return 0;
 }
 
